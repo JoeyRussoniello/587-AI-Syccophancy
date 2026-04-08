3e2a8471c8f21c704f42b03d77f0eb5e56c15588
JoeyRussoniello <jmrusso@bu.edu>
2026-04-07 22:58:27 -0400

    Added ratio calculation query

---
+++ database/queries/Calculate_NTA_Ratios.sql
@@ -0,0 +1,12 @@
+WITH RESPONSE_COUNTS AS (
+    SELECT 
+		model,
+		system_prompt,
+        SUM(CASE WHEN llm_label = 'NTA' THEN 1 ELSE 0 END) AS NTA_COUNT,
+        SUM(CASE WHEN llm_label != 'NTA' THEN 1 ELSE 0 END) AS YTA_COUNT
+    FROM llm_responses AS R
+	INNER JOIN system_prompts AS P
+	ON R.system_prompt_id = P.system_prompt_id
+	GROUP BY model, system_prompt
+)
+SELECT *, CAST(NTA_COUNT AS FLOAT) / CAST(YTA_COUNT AS FLOAT) AS NTA_RATIO FROM RESPONSE_COUNTS;
